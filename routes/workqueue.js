d6451fe03e1c57afc92d60796d7b9fdb5b00277a
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2020-05-09 22:55:42 +0200

    workqueue edited

--- routes/workqueue.js
+++ routes/workqueue.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 router.get('/view', function (req, res, next) {
     res.render('workQueue', {
-        title: 'list of work queue'
+        title: 'list of work queue',
+        user: req.user
     });
 });
 module.exports = router;
