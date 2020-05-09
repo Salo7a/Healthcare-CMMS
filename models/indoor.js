a83ba923f64e9dc374feb9c3b3850e9637296f70
Ahmad <ahmedmageed19@gmail.com>
2020-05-09 22:14:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- models/indoor.js
+++ models/indoor.js
@@ -1,10 +1,19 @@
 'use strict';
+const models = require("../models");
+
 module.exports = (sequelize, DataTypes) => {
     const Indoor = sequelize.define('Indoor', {
-        firstName: DataTypes.STRING
+        firstName: DataTypes.STRING,
+        lastName : DataTypes.STRING,
+        birthday : DataTypes.DATE,
+        SSN : DataTypes.UInt16,
+        Role : DataTypes.BOOL,
+        WorkHours : DataTypes.Int8
     }, {});
+
     Indoor.associate = function (models) {
         // associations can be defined here
+        Indoor.belongsTo(models.devices, )
     };
     return Indoor;
 };
---
+++ routes/indoor.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid");
+const isAuth = require("../utils/filters").isAuth
+
+router.get('/', isAuth, (req, res)=>{
+    res.render("../views/indoor.ejs")
+})
+
+
