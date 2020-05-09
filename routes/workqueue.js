389da4e8ab603b798f08d8237ef5d55c00128678
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2020-05-09 20:32:17 +0200

    edited route

--- routes/workqueue.js
+++ routes/workqueue.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-router.get('/portal/workqueue', function (req, res, next) {
+router.get('/view', function (req, res, next) {
     res.render('workQueue', {
         title: 'list of work queue'
     });
