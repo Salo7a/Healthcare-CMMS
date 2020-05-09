28871950cace8672390f557d4f83c7dfd0adeee3
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2020-05-09 18:43:44 +0200

    workqueue made

--- models/workqueue.js
+++ models/workqueue.js
@@ -1,10 +1,26 @@
 'use strict';
+const models = require('../models');
+
 module.exports = (sequelize, DataTypes) => {
     const WorkQueue = sequelize.define('WorkQueue', {
-        firstName: DataTypes.STRING
+        task_name: DataTypes.STRING,
+        task_date: DataTypes.STRING,
+        task_priority: DataTypes.STRING
     }, {});
     WorkQueue.associate = function (models) {
         // associations can be defined here
+        WorkQueue.hasMany(models.WorkOrder,{
+        as: 'workorder'
+        });
+        WorkQueue.belongsTo(models.User,{
+            as: 'engineer',
+            foreignKey: 'EngineerId',
+            targetKey: 'id'
+        });
+        WorkQueue.hasMany(models.Device,{
+            as: 'devices'
+
+        });
     };
     return WorkQueue;
 };
