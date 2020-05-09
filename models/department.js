09e17eba46c7da0dc085feeae8fc1144e412616c
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-05-10 00:32:30 +0200

    Added Department Model

---
+++ models/department.js
@@ -0,0 +1,17 @@
+'use strict';
+const models = require('../models');
+
+module.exports = (sequelize, DataTypes) =>
+{
+    const Department = sequelize.define('Department', {
+        // attributes
+        Name: DataTypes.STRING
+    }, {});
+    
+    Department.associate = function (models) {
+        // associations can be defined here
+        Department.hasMany(models.Device);
+    };
+    
+    return Department;
+};
