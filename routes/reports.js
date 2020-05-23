4c2182b42df8cc948320d09205a1bbb3ac396cad
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-05-24 01:17:11 +0200

    Edit Reports Extraction Page

--- routes/reports.js
+++ routes/reports.js
@@ -13,33 +13,11 @@ router.get('/', isAuth, (req, res, next) => {
     });
 });
 
-// GET Route Handler for repair report page
-router.get('/repair', isAuth, (req, res, next) => {
-    res.render('reports/repair', {
-        title: 'Repair Report'
+// GET Route Handler for create report page
+router.get('/createReport', isAuth, (req, res, next) => {
+    res.render('reports/createReport', {
+        title: 'Reports Extraction'
     });
 });
-
-// GET Route Handler for ppm report page
-router.get('/ppm', isAuth, (req, res, next) => {
-    res.render('reports/ppm', {
-        title: 'PPM Report'
-    });
-});
-
-// GET Route Handler for daily inspection report page
-router.get('/daily', isAuth, (req, res, next) => {
-    res.render('reports/daily', {
-        title: 'Daily Inspection Report'
-    })
-});
-
-// GET Route Handler for adding a new Device
-router.post('/add', isAuth, (req, res) => {
-    res.render('reports/add', {
-        title: 'Get The Report'
-    });
-});
-
 
 module.exports = router;
