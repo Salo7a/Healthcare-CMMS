e491a1d2576e7646a86151f6d4f5a1a8f7927b5c
AMahdy98 <ahmadmahdy3098@gmail.com>
2020-05-25 06:10:32 +0200

    parts mock data

--- routes/parts.js
+++ routes/parts.js
@@ -13,7 +13,7 @@ router.get('/', isAuth, (req, res) => {
         parts => {
             res.render('parts/index', {
                 title: 'Parts List',
-                parts
+                parts : parts
             }).catch((error) => {
                 console.log(error.toString());
                 res.status(400).send(error);
@@ -56,7 +56,7 @@ router.post('/delete', isAdmin, (req, res) => {
 });
 
 //POST Route Handler for Deleting all part
-router.get('deleteAll', isAdmin, (req, res) => {
+router.get('/deleteAll', isAdmin, (req, res) => {
     parts.destroy({
         where: {}
     }).then(() => {
